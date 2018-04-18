ff3918b7b773ea7eb38a5b4ce9de7b61fe10b4c4
duheran <dduheran@gmail.com>
2018-04-18 20:17:47 +0800

    add transpose dtan2

--- prcv_expreiment/trainging/dtan_tanspose_train2.py
+++ prcv_expreiment/trainging/dtan_tanspose_train2.py
@@ -127,12 +127,12 @@ def run_training(fold_num, train_tfrecord_path, test_tfrecord_path, train_batch_
         # saver = tf.train.Saver()
 
         # Create a session for running Ops on the Graph.
-        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.5)
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.47)
         with tf.Session(config=tf.ConfigProto(allow_soft_placement=True, log_device_placement=True, gpu_options= gpu_options)) as sess:
 
             # Instantiate a SummaryWriter to output summaries and the Graph.
-            train_writer = tf.summary.FileWriter('./summaries_new/summaries_graph_0418(2)/'+str(fold_num)+'/train', sess.graph)
-            test_writer = tf.summary.FileWriter('./summaries_new/summaries_graph_0418(2)/'+str(fold_num)+'/test', sess.graph)
+            train_writer = tf.summary.FileWriter('./summaries_new/summaries_graph_0418(3)/'+str(fold_num)+'/train', sess.graph)
+            test_writer = tf.summary.FileWriter('./summaries_new/summaries_graph_0418(3)/'+str(fold_num)+'/test', sess.graph)
 
             # And then after everything is built:
 
