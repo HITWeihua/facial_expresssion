5ae0deb7bda5994cf2309941143b609f2e6aa139
dduheran_ubuntuc <dduheran@gmail.com>
2018-06-09 21:02:29 +0800

    CASIA NIR mtcnn face extractor

--- utils/face_extractor_nis_mtcnn.py
+++ utils/face_extractor_nis_mtcnn.py
@@ -13,18 +13,39 @@ spliter = re.compile(r'[\(\)\,\n]+')
 
 # base_read_path = os.path.abspath('F:\\files\\facial_expresssion\\oulu\\Strong\\P013\\Disgust')
 # base_write_path = os.path.abspath('../oulu/oulu_face_landmark')
-base_read_path = os.path.abspath('F:\\files\\人脸识别相片\\人脸识别切割\\174936')
-base_write_path = os.path.abspath('F:\\files\\人脸识别相片\\cropped\\174936')
-
-files = [f for f in os.listdir(base_read_path) if '.jpeg' in f]
+base_read_path = os.path.abspath('/home/duheran/OriginalImg/NI/Strong')
+base_write_path = os.path.abspath('/home/duheran/OriginalImg/NI/crop')
+person_numbers = os.listdir(base_read_path)
+# files = [f for f in os.listdir(base_read_path) if '.jpeg' in f]
 detector = MTCNN()
 
-for file_num in range(len(files)):
-    read_f_path = os.path.join(base_read_path, files[file_num])
-    write_f_path = os.path.join(base_write_path, files[file_num])
-    print("Processing file: {}".format(read_f_path))
-    img = cv2.imread(read_f_path)
-    print(detector.detect_faces(img))
+for person in person_numbers:
+    read_f_path = os.path.join(base_read_path, person)
+    write_f_path = os.path.join(base_write_path, person)
+    if not os.path.isdir(write_f_path):
+        os.mkdir(write_f_path)
+    exps = os.listdir(read_f_path)
+    for exp in exps:
+        read_i_path = os.path.join(read_f_path, exp)
+        write_i_path = os.path.join(write_f_path, exp)
+        if not os.path.isdir(write_i_path):
+            os.mkdir(write_i_path)
+        images = [k for k in os.listdir(read_i_path) if '.jpeg' in k]
+        for i in images:
+            read_path = os.path.join(read_i_path, i)
+            write_path = os.path.join(write_i_path, i)
+            print("Processing file: {}".format(read_path))
+            img = cv2.imread(read_path)
+            box = detector.detect_faces(img)[0]['box']
+            img_crop = img[box[1]:box[1]+box[3], box[0]:box[0]+box[2] , :]
+            cv2.imwrite(write_path, img_crop)
+
+# for file_num in range(len(files)):
+#     read_f_path = os.path.join(base_read_path, files[file_num])
+#     write_f_path = os.path.join(base_write_path, files[file_num])
+#     print("Processing file: {}".format(read_f_path))
+#     img = cv2.imread(read_f_path)
+#     print(detector.detect_faces(img))
 
 
 
